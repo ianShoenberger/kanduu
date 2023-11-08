88d040bcf99c83bf4babfba0d7042ad548096879
Ian Shoenberger <ian.shoenberger@fujifilm.com>
2023-11-08 11:17:38 -0500

    converted app to pwa

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite'
 import {BootstrapVueNextResolver} from 'unplugin-vue-components/resolvers'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,5 +11,41 @@ export default defineConfig({
     Components({
       resolvers: [BootstrapVueNextResolver()],
     }),
+    VitePWA({
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+      },
+      manifest: {
+        includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'fakeIcon.svg'],
+        name: 'Kanduu',
+        short_name: 'Kanduu',
+        description: 'Manage the stuff you like to do',
+        theme_color: '#eb8934',
+        icons: [
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any'  
+          },
+          {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ]
+      }
+    })
   ],
 })
