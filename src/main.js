72281dca504bacb6ad19bee743842a2c0256d81a
Ian Shoenberger <ian.shoenberger@fujifilm.com>
2024-09-06 11:48:38 -0400

    check for updates every 2 minutes

--- src/main.js
+++ src/main.js
@@ -8,12 +8,34 @@ import App from "./App.vue";
 import { registerSW } from 'virtual:pwa-register'
 
 const showRefreshPrompt = ref(false)
+const intervalMS = 1000 * 60 * 2
+
 const updateSW = registerSW({
+  onRegisteredSW(swUrl, r) {
+    r && setInterval(async () => {
+      if (r.installing || !navigator)
+        return
+
+      if (('connection' in navigator) && !navigator.onLine)
+        return
+
+      const resp = await fetch(swUrl, {
+        cache: 'no-store',
+        headers: {
+          'cache': 'no-store',
+          'cache-control': 'no-cache',
+        },
+      })
+
+      if (resp?.status === 200)
+        await r.update()
+    }, intervalMS)
+  },
   onNeedRefresh() {
     showRefreshPrompt.value = true
   },
   onOfflineReady() {},
-});
+})
 
 const app = createApp(App);
 app.use(createPinia()).use(router);
